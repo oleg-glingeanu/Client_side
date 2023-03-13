2f7d21edb0b16990d0818be05c0c42c78f95c27b
Oleg Glingeanu <oleg.glingeanu@gmail.com>
2023-03-13 15:07:32 +0000

    up
    up

--- src/Components/Posts/PostCard.jsx
+++ src/Components/Posts/PostCard.jsx
@@ -12,7 +12,7 @@ export default function PostCard({post}) {
         <Card>
           <CardMedia component='img'
               height='140'
-              image={`http://localhost:3001/assets/${post.picturePath}`}>
+              image={`4thyearproject-production.up.railway.app/assets/${post.picturePath}`}>
           </CardMedia>
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
